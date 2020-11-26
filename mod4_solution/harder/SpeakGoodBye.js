a6e7c6c9f9417cf23fc7703c31babe4427a69057
Ali Hamdan <ashamdan1990@gmail.com>
2020-11-26 16:48:40 +0200

    Removing comments from speakGoodbye

--- mod4_solution/harder/SpeakGoodBye.js
+++ mod4_solution/harder/SpeakGoodBye.js
@@ -1,7 +1,3 @@
-// NOTE! The steps in this file are basically identical to the ones you
-// performed in the SpeakHello.js file.
-
-// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 (function(window){
 var speakWord = "Good Bye";
 var byeSpeaker = new Object();
